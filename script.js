e4ac5dbd517cc1358987be6b344d6d912f162e8b
sakibpro2003 <sakibprodhan2003@gmail.com>
2024-02-18 18:10:27 +0600

    seat count dynamic

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert("connected");
+// Booked seat count
+let selectedSeatCount = 0;
+const availableSeatCount =8;
+
+
+function nowclick(id,className){
+    console.log(id);
+    const bgColor = document.getElementById(id);
+    bgColor.style.backgroundColor = '#1DD100';
+    selectedSeatCount ++;
+    const disableBtn = document.getElementsByClassName(className);
+        if(selectedSeatCount===4){
+            for(let btn of disableBtn){
+            
+            btn.disabled= true;
+            }
+        }
+
+    const getSeatCount = document.getElementById('seatCount');
+    getSeatCount.innerText = selectedSeatCount;  
+    
+    
+    // console.log(selectedSeatCount);
+    
+}
+    
+
